94a401579ee4b5b833cd5ea467179dfdeaaac6c8
Hae-jinYu <98142219+Hae-jinYu@users.noreply.github.com>
2023-07-24 09:41:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/chargecalculater/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.chargecalculater
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.chargecalculater.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val items = resources.getStringArray(R.array.family_array)
+
+    //val Adapter1 = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
