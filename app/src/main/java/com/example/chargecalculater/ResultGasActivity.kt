8d0ccf319dfb0228876a04fca72a87c1197b8c78
Hae-jinYu <98142219+Hae-jinYu@users.noreply.github.com>
2023-07-26 13:58:56 +0900

    gas design1

---
+++ app/src/main/java/com/example/chargecalculater/ResultGasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chargecalculater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultGasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_gas)
+    }
+}
--- app/src/main/java/com/example/chargecalculater/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/chargecalculater/ui/notifications/NotificationsFragment.kt
@@ -1,12 +1,17 @@
 package com.example.chargecalculater.ui.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.chargecalculater.R
+import com.example.chargecalculater.ResultElectric
+import com.example.chargecalculater.ResultGasActivity
 import com.example.chargecalculater.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
@@ -28,10 +33,19 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.basebill
         notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            textView.text = ""
         }
+
+        val resultgasButton: Button = binding.resultgasbutton
+        resultgasButton.findViewById<Button>(R.id.resultgasbutton)
+
+        resultgasButton.setOnClickListener {
+            var intent = Intent(context, ResultGasActivity::class.java)
+            startActivity(intent)
+        }
+
         return root
     }
 
