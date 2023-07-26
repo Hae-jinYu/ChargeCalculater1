65d36d38536bd165c3ac0784c9271a5c6ae6d2d4
2yeb <qls7404@naver.com>
2023-07-26 15:47:54 +0900

    수정

--- app/src/main/java/com/example/chargecalculater/MainActivity.kt
+++ app/src/main/java/com/example/chargecalculater/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
